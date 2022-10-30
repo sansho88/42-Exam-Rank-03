0c66529f8ec9573697284e027376ced2640b3461
fdaumas <florian.daumas.git@gmail.com>
2022-10-30 22:06:21 +0100

    gnl

---
+++ gnl/get_next_line.c
@@ -0,0 +1,10 @@
+#include "get_next_line.h"
+#include <unistd.h>
+#include <stdlib.h>
+
+char *get_next_line(int fd)
+{
+	char *s = malloc(10000), *c = s;
+	while(read(fd, c, 1) > 0 && *c++ != '\n');
+	return c > s ? (*c = 0, s) : (free(s), NULL);
+}
