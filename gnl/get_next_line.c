c0a1a60f0e82ddc25d0802bb2f3e3b32e6b3c4cb
fdaumas <florian.daumas.git@gmail.com>
2022-10-31 23:08:55 +0100

    get_next_line for understanding how its work

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -2,9 +2,26 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-char *get_next_line(int fd)
+char *get_next_line_3_ligne(int fd)
 {
 	char *s = malloc(10000), *c = s;
 	while(read(fd, c, 1) > 0 && *c++ != '\n');
 	return c > s ? (*c = 0, s) : (free(s), NULL);
+}
+
+char	*get_next_line(int fd)
+{
+	char *str = malloc (10000);
+	char *c = str;									// c pointe sur str pour ecrire directement dans str
+	while (read(fd, c, 1) > 0 && *c++ != '\n');		// read de 1 caractere dans c soit 1er element de str si read != 0 pb sa stop && si *c == \n on stop et *c++ pour que le prochain buffer du read soit sur la prochaine address de str
+	if (c > str)									// si c > str cela veut dire que l'on a ecrit donc des choses a renvoyer
+	{
+		*c = 0;										// cleanup avant return
+		return (str);
+	}
+	else
+	{
+		free(str);									// cleanup avant return
+		return (NULL);
+	}
 }
